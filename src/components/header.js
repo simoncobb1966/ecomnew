842d7202eb6adca598219170beab3dcba01736ae
simoncobb1966 <48588635+simoncobb1966@users.noreply.github.com>
2021-05-12 18:04:28 +0100

    banner image amended

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Header extends Component {
-
-    render() {
-
-        return (
-
-                    <div className="row banner">
-                            <img className="img-fluid borderdiv bannerrow" src="https://jb-shop.s3.eu-west-2.amazonaws.com/jb_shop_site_images/top-banner.png" alt="banner"></img>
-                        </div>  
-
-        )
-    }
+  render() {
+    return (
+      <div className="row banner">
+        <img
+          className="img-fluid borderdiv bannerrow"
+          src="https://www.simoncobb.co.uk/jbshop2/images/jb_banner.png"
+          alt="banner"
+        ></img>
+      </div>
+    );
+  }
 }
 export default Header;
